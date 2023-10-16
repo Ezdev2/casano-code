e3d11d09663f48761747377dd69fae0eb00ce9d1
ezdev2 <fansoaezra@gmail.com>
2023-10-16 12:08:27 +0300

    fix contact.php

--- docs/contact.php
+++ docs/contact.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // Envoyez l' email
   $sujet = 'Nouveau message de contact';
   $message = "Nom : $name\nEmail : $email\nMessage : $message";
-  mail('', $sujet, $message);
+  mail('contact@casano-agency.com', $sujet, $message);
 
   echo '<p>Votre message a été envoyé.</p>';
 }
